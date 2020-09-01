88d1a868d460212fbb628f3b0b43ff6fe38c3341
jurdin32 <urdin-23@live.com>
2020-09-01 18:20:41 -0500

    Configuracion inicial

--- Home/models.py
+++ Home/models.py
@@ -17,4 +17,5 @@ class Proyectos(models.Model):
     categoria=models.ForeignKey(Categoria, on_delete=models.CASCADE)
     nombre=models.CharField(max_length=60)
     imagen=models.FileField(upload_to="projects/")
+    url=models.URLField()
     autor=models.ForeignKey(Autores, on_delete=models.CASCADE)
