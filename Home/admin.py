8d7fc35db16c73cadca3aeb2dc870c69e2c4600b
jurdin32 <urdin-23@live.com>
2020-09-01 18:36:20 -0500

    Configuracion inicial

--- Home/admin.py
+++ Home/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from Dizof.snippers import Attr
 from Home.models import *
 
-admin.site.register(Categoria)
-admin.site.register(Autores)
-admin.site.register(Proyectos)
+@admin.register(Categoria)
+class AdminCategorias(admin.ModelAdmin):
+    list_display_links = Attr(Categoria)
+    list_display = Attr(Categoria)
+
+@admin.register(Autores)
+class AdminAutores(admin.ModelAdmin):
+    list_display_links = Attr(Autores)
+    list_display = Attr(Autores)
+
+@admin.register(Proyectos)
+class AdminProyectos(admin.ModelAdmin):
+    list_display_links = Attr(Proyectos)
+    list_display = Attr(Proyectos)
+
+@admin.register(Principal)
+class AdminPrincipal(admin.ModelAdmin):
+    list_display_links = Attr(Principal)
+    list_display = Attr(Principal)
