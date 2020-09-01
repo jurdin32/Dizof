5fd8d705f6a8cc0092b8bd4793defe8e38045ebb
jurdin32 <urdin-23@live.com>
2020-09-01 18:39:02 -0500

    Configuracion inicial

--- Home/views.py
+++ Home/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 
-from Home.models import Categoria, Proyectos
+from Home.models import Categoria, Proyectos, Principal
 
 
 def index(request):
-    return render(request,"index.html")
+    contexto={
+        "principal": Principal.objects.last(),
+    }
+    return render(request,"index.html",contexto)
 
 def proyectos(request):
     proyectos=Proyectos.objects.all()
@@ -19,6 +22,7 @@ def proyectos(request):
         "categorias":Categoria.objects.all(),
         "proyectos":proyectos,
         "categoria":categoria,
+        "principal":Principal.objects.last(),
     }
 
     return render(request,"proyectos.html",contexto)
