1ab43c86fb77973815308d702aecc85136d87da1
jurdin32 <urdin-23@live.com>
2020-09-01 09:54:52 -0500

    Configuracion inicial

--- Dizof/urls.py
+++ Dizof/urls.py
@@ -18,11 +18,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from Home.views import Index
+from Home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',Index.as_view(), name="index")
+    path('',index.as_view(), name="index"),
+    path('', proyectos.as_view(), name="proyectos")
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Administración de DIzof'
