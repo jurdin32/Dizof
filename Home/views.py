f0b4670f85210bc8452bd2dc0adddccc5ec4baa2
Johnny <urdin32@gmail.com>
2020-11-02 10:54:08 -0500

    30/10/2020

--- Home/views.py
+++ Home/views.py
@@ -14,6 +14,7 @@ def index(request):
         "servicios":Servicios.objects.all(),
         "historia":Historia.objects.filter(tipo="Historia").first(),
         "pasion":Historia.objects.filter(tipo="Pasion").first(),
+
     }
     return render(request,"index.html",contexto)
 
